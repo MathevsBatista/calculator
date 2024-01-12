f76412fa09b81820020455a2fa376a1d27a2703a
MathevsBatista <matheusisidoro09@gmail.com>
2024-01-12 19:50:59 -0300

    Adds calculator with all functions working

---
+++ calculator.js
@@ -0,0 +1,105 @@
+function add(number1, number2) {
+    return +number1 + +number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    if(number2 == 0){
+	return "Não é possível dividir por zero!";
+    }
+    return number1 / number2;
+}
+
+function operate(number1, operator, number2) {
+    switch(operator) {
+	case "+":
+	    return add(number1, number2);
+	case "-":
+	    return subtract(number1, number2);
+	case "*":
+	    return multiply(number1, number2);
+	case "/":
+	    return divide(number1, number2);
+	default:
+	    return "Invalid operator!"
+    }
+}
+
+function calculator(numberOperatorNumberArray) {
+    const numberOperatorNumber = [];
+    const numberOperatorNumberArrayReversed = numberOperatorNumberArray.reverse();
+
+    for (let i = 0; numberOperatorNumberArrayReversed.length > 0; i++) {
+	numberOperatorNumber.push(numberOperatorNumberArrayReversed.pop());
+
+	if(numberOperatorNumber.length == 3) {
+	    i = 0;
+	    result = parseFloat(operate(...numberOperatorNumber).toFixed(2));
+
+	    numberOperatorNumber.length = 0;
+	    numberOperatorNumber.push(result);
+	}
+    }
+
+    return numberOperatorNumber[0];
+}
+
+const displayLastResult = document.querySelector(".display-last-result")
+const displayResult = document.querySelector(".display-result")
+
+const numberButtons = document.querySelectorAll(".numbers button");
+const deleteButton = document.querySelector(".delete");
+const clearButton = document.querySelector(".clear");
+const operationButtons = document.querySelectorAll(".operators button");
+const resultButton = document.querySelector(".result-button");
+let numberOperatorNumberString = "";
+
+numberButtons.forEach( (numberButton) => {
+    numberButton.addEventListener("click", () => {
+	numberOperatorNumberString += numberButton.textContent;
+	const regexRemoveSpaceBetweenNumbers = /(\d)\s+(?=\d)/g;
+	numberOperatorNumberString = numberOperatorNumberString.replace(regexRemoveSpaceBetweenNumbers, "$1");
+
+	displayResult.textContent = numberOperatorNumberString.trim();
+    } );
+} );
+
+operationButtons.forEach( (operationButton) => {
+    operationButton.addEventListener("click", () => {
+	let numberOperatorNumber = numberOperatorNumberString.trim().split(" ");
+
+	if(numberOperatorNumber.length == 3){
+	    displayLastResult.textContent = displayResult.textContent;
+
+	    displayResult.textContent = numberOperatorNumberString = calculator(numberOperatorNumber);
+	}
+
+	numberOperatorNumberString += " " + operationButton.textContent + " ";
+	displayResult.textContent = numberOperatorNumberString.trim();
+    } );
+} );
+
+deleteButton.addEventListener("click", () => {
+    numberOperatorNumberString = numberOperatorNumberString.trim().slice(0, -1);
+    
+    displayResult.textContent = numberOperatorNumberString;
+} );
+
+clearButton.addEventListener("click", () => {
+    numberOperatorNumberString = "";
+    displayResult.textContent = "";
+} );
+
+resultButton.addEventListener("click", () => {
+    displayLastResult.textContent = displayResult.textContent;
+    displayResult.textContent = calculator(numberOperatorNumberString.split(" "));
+    numberOperatorNumberString = "";
+} );
+
